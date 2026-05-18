68ab19acca8849820d77cae5b094433d27e81cf7
mjcw88 <144539369+mjcw88@users.noreply.github.com>
2026-05-18 14:06:16 +0100

    added isValid function

--- functions/mergeSort/mergeSort.js
+++ functions/mergeSort/mergeSort.js
@@ -1,24 +1,29 @@
-export function mergeSort(array) {
-    function merge(left, right) {
-        const sortedArray = [];
-        let i = 0;
-        let j = 0;
+function isValid(array) {
+    if (!Array.isArray(array)) return false;
+    if (array.some((x) => typeof x !== "number") || array.length === 0) return false;
+    return true;
+}
 
-        while (i < left.length && j < right.length) {
-            if (left[i] < right[j]) {
-                sortedArray.push(left[i]);
-                i += 1;
-            } else {
-                sortedArray.push(right[j]);
-                j += 1;
-            }
+function merge(left, right) {
+    const sortedArray = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < left.length && j < right.length) {
+        if (left[i] < right[j]) {
+            sortedArray.push(left[i]);
+            i += 1;
+        } else {
+            sortedArray.push(right[j]);
+            j += 1;
         }
-        sortedArray.push(...left.slice(i), ...right.slice(j));
-        return sortedArray;
     }
+    sortedArray.push(...left.slice(i), ...right.slice(j));
+    return sortedArray;
+}
 
-    if (!Array.isArray(array)) return; // Checks if input is an array
-    if (array.some((x) => typeof x !== "number") || array.length === 0) return; // Checks if all values inside array are valid
+export function mergeSort(array) {
+    if (!isValid(array)) return;
     if (array.length === 1) return array;
 
     const middle = Math.ceil(array.length / 2);
@@ -27,6 +32,6 @@ export function mergeSort(array) {
 
     const sortedLeft = mergeSort(leftSide);
     const sortedRight = mergeSort(rightSide);
-
+    
     return merge(sortedLeft, sortedRight);
 }
